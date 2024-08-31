4f0d1cceabaed8d96c30a6dad5f470a090e4fee8
Fernanda Sandoval <luisa.sandoval@commity.co>
2024-08-30 19:57:58 -0500

    Add user model

---
+++ src/models/user.js
@@ -0,0 +1,34 @@
+const mongoose = require('mongoose');
+
+// Definir el esquema de usuario
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    role: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    }
+});
+
+// Crear un modelo basado en el esquema
+const User = mongoose.model('User', userSchema);
