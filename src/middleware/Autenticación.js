340de8d76a534aa7fc074a119c1c7f753e55fd6b
The-greenpanter <judpenaca@unal.edu.co>
2024-09-20 07:26:18 -0500

    authetication

---
+++ src/middleware/Autenticación.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const User = require('../models/User'); // Asegúrate de que la ruta sea correcta
+
+// Middleware para autenticar el token
+const authenticate = (req, res, next) => {
+    const token = req.headers['authorization']?.split(' ')[1]; // Obtener el token del encabezado
+
+    if (!token) {
+        return res.status(401).json({ message: 'Access Denied: No token provided' });
+    }
+
+    jwt.verify(token, process.env.JWT_SECRET, async (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: 'Invalid Token' });
+        }
+
+        // Almacenar la información del usuario en la solicitud
+        req.user = await User.findById(decoded.id).select('-password'); // Excluir la contraseña
+        next();
+    });
+};
+
+module.exports = { authenticate };
