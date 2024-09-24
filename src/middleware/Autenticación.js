0a3926e9c75bc7dc60f0194c67b2293409f2c846
The-greenpanter <judpenaca@unal.edu.co>
2024-09-24 18:40:28 -0500

    Categories ready

--- src/middleware/Autenticación.js
+++ src/middleware/Autenticación.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const User = require('../models/User');
+const User = require('../models/User.js');
 
 // Middleware para autenticar el token
 const authenticate = (req, res, next) => {
