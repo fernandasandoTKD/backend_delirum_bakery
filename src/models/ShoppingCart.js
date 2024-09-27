9fa9f9d6ac7063d6b2c3e26a850077aea2177ac5
The-greenpanter <judpenaca@unal.edu.co>
2024-09-27 17:08:34 -0500

    Ready to Pull

--- src/models/ShoppingCart.js
+++ src/models/ShoppingCart.js
@@ -20,4 +20,4 @@ const shoppingCartSchema = new mongoose.Schema({
     }],
 }, { timestamps: true });
 
-module.exports = mongoose.model('ShoppingCart', shoppingCartSchema)
+module.exports = mongoose.model('ShoppingCart', shoppingCartSchema);
